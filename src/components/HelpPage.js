c39a4ac299fbf12951fb7a888e0b49460f2a3dc2
Nick Randall <nickrandall@Nicks-Air.telecom>
2018-04-13 09:42:09 +1200

    Add private routes

--- src/components/HelpPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-
-const HelpPage = () => (
-    <div>This is from my help expense component</div>
-);
-
-export default HelpPage;
